99dcfb82bcb3f7024eeae54fcf0229849e42ba67
Ben Llewelyn <bencon@udel.com>
2022-01-14 16:34:37 -0600

    Implement Requirements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+function Change(props) {
+    const totalChange = props.provided - props.cost;
+
+    if (totalChange < 0) {
+        return (
+            <div>
+                Please provide adequate money!
+            </div>
+        );
+    }
+
+    let changeDollarsRemaining =  Math.floor(totalChange);
+    let changeCentsRemaining =  Math.round((totalChange - changeDollarsRemaining) * 100);
+
+    const billsTen = Math.floor(changeDollarsRemaining / 10);
+    changeDollarsRemaining -= billsTen * 10;
+    const billsFive = Math.floor(changeDollarsRemaining / 5);
+    changeDollarsRemaining -= billsFive * 5;
+    const billsOne = changeDollarsRemaining;
+
+    const quarters = Math.floor(changeCentsRemaining / 25);
+    changeCentsRemaining -= quarters * 25;
+    const dimes = Math.floor(changeCentsRemaining / 10);
+    changeCentsRemaining -= dimes * 10;
+    const nickels = Math.floor(changeCentsRemaining / 5);
+    changeCentsRemaining -= nickels * 5;
+    const pennies = Math.round(changeCentsRemaining);
+
+    const allDenominations = [];
+    allDenominations.push({denonimation: '$10 bill', quantity:billsTen});
+    allDenominations.push({denonimation: '$5 bill', quantity:billsFive});
+    allDenominations.push({denonimation: '$1 bill', quantity:billsOne});
+    allDenominations.push({denonimation: 'quarters', quantity:quarters});
+    allDenominations.push({denonimation: 'dimes', quantity:dimes});
+    allDenominations.push({denonimation: 'nickels', quantity:nickels});
+    allDenominations.push({denonimation: 'pennies', quantity:pennies});
+
+    return (
+        <React.Fragment>
+            <ul>
+                {
+                    allDenominations.map(function(item, index){
+                        return (
+                            <li key={index}>
+                                {`${item.denonimation} : ${item.quantity}`}
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+        </React.Fragment>
+   );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [input, setInput] = useState({"totalCost":0, "amountProvided":0});
+
+    const handleInputChange = (e) => {
+        setInput({
+            ...input,
+            [e.currentTarget.name]: e.currentTarget.value
+        });
+    };
+
+    return (
+        <div className='myForm'>
+            <label>
+                Total Cost (USD):
+                <input
+                    type="number"
+                    name="totalCost"
+                    onChange={handleInputChange}
+                    placeholder="0"
+                />
+            </label>
+            <br />
+            <label>
+                Amount Provided (USD):
+                <input
+                    type="number"
+                    name="amountProvided"
+                    onChange={handleInputChange}
+                    placeholder="0"
+                />
+            </label>
+            <Change cost={input.totalCost} provided={input.amountProvided} />
+        </div>
+    );
 }
 
 export default App;
